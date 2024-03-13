e86aca04bcb14da49c25be5a934d1a5a6f4d3dcc
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-03-13 07:29:36 +0000

    add product details page

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-sarahgoodwi-boutiqueado-2z4rlg54bt2.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['8000-sarahgoodwi-boutiqueado-2z4rlg54bt2.ws-eu110.gitpod.io']
 
 
 # Application definition
